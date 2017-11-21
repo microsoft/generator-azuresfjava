d142278797ee2bc808bc35f45bfd8e53e17dab1a
Sayantan Saha <saysa@microsoft.com>
2017-11-21 14:13:42 +0530

    Stateful skeleton update (#5)
    * Updating statefulservice skeleton to make it more readable
    * updating yeoman java version

--- generators/ReliableStatefulService/templates/class/Service.java
+++ generators/ReliableStatefulService/templates/class/Service.java
@@ -11,6 +11,7 @@ import microsoft.servicefabric.data.ReliableStateManager;
 import microsoft.servicefabric.data.Transaction;
 import microsoft.servicefabric.services.communication.runtime.ServiceReplicaListener;
 import microsoft.servicefabric.services.runtime.StatefulService;
+import microsoft.servicefabric.data.collections.ReliableHashMap;
 import system.fabric.CancellationToken;
 import system.fabric.StatefulServiceContext;
 
@@ -33,30 +34,42 @@ public class <%= serviceClassName %> extends StatefulService {
 
     @Override
     protected CompletableFuture<?> runAsync(CancellationToken cancellationToken) {
-    // TODO: Replace the following sample code with your own logic 
-    // or remove this runAsync override if it's not needed in your service.
-    Transaction tx = stateManager.createTransaction();
-    return this.stateManager.<String, Long>getOrAddReliableHashMapAsync("myHashMap").thenCompose((map) -> {
+        // TODO: Replace the following sample code with your own logic
+        // or remove this runAsync override if it's not needed in your service.
+
+        Transaction tx = stateManager.createTransaction();
+        CompletableFuture<ReliableHashMap<String, Long>> mapFuture = this.stateManager
+                .<String, Long>getOrAddReliableHashMapAsync("myHashMap");
+
+        return mapFuture.thenCompose((map) -> {
+            return computeValueAsync(map, tx, cancellationToken);
+        }).thenCompose((v) -> {
+            return commitTransaction(tx);
+        }).whenComplete((res, e) -> {
+            closeTransaction(tx);
+        });
+    }
+
+    private static CompletableFuture<Long> computeValueAsync(ReliableHashMap<String, Long> map, Transaction tx,
+            CancellationToken token) {
         return map.computeAsync(tx, "counter", (k, v) -> {
             if (v == null)
                 return 1L;
             else
                 return ++v;
-        }, Duration.ofSeconds(4), cancellationToken).thenApply((l) -> {
-            return tx.commitAsync().handle((r, x) -> {
-                if (x != null) {
-                    logger.log(Level.SEVERE, x.getMessage());
-                }
-                try {
-                    tx.close();
-                } catch (Exception e) {
-                    logger.log(Level.SEVERE, e.getMessage());
-                }
-                return null;
-            });
-        });
-    });
+        }, Duration.ofSeconds(4), token);
+    }
 
+    private static CompletableFuture<?> commitTransaction(Transaction tx) {
+        return tx.commitAsync();
+    }
+
+    private static void closeTransaction(Transaction tx) {
+        try {
+            tx.close();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Exception in closing transaction", e);
+        }
     }
 }
 
